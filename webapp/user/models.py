00d9ab4f8ebbd18be5869ed6fbd5bf5d2c8a4b9b
fazledyn <1705066@ugrad.cse.buet.ac.bd>
2021-02-17 23:02:34 +0600

    Pip freeze, user.models.py changed

--- webapp/user/models.py
+++ webapp/user/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class TicketUser(models.Model):
+
+    id = models.IntegerField(primary_key=True)
+    email = models.EmailField(null=False)
+    password = models.TextField(null=False)
+
+    full_name = models.TextField(null=False)
+    unique_hash = models.TextField()
+
+
+    def __str__(self):
+        return `Userid: ${id}, Email: ${email}`
+
