2fd2d08af97d04ab107f5a90b341e5f7d253ba35
fazledyn <1705066@ugrad.cse.buet.ac.bd>
2021-02-19 00:16:14 +0600

    TicketUser login, signup, dashboard added

--- webapp/ticketuser/models.py
+++ webapp/ticketuser/models.py
@@ -3,16 +3,16 @@ import hashlib
 
 
 class TicketUser(models.Model):
-
-    id = models.IntegerField(primary_key=True)
+    id = models.IntegerField(auto_created=True, primary_key=True)
     email = models.EmailField(null=False, unique=True)
     password = models.TextField(null=False)
 
     full_name = models.TextField(null=False)
     unique_hash = models.TextField()
 
-    def __init__(self):
-        self.unique_hash = hashlib.sha256(self.email, self.full_name).hexdigest()
+    def save(self, *args, **kwargs):
+        self.unique_hash = hashlib.sha256("".join([self.email, self.full_name]).encode()).hexdigest()
+        super(TicketUser, self).save(*args, **kwargs)
 
     def __str__(self):
         return "Ticket Userid: {}".format(self.id)
