1871318b2a01ea5be56ac35663e097a53bc1e89a
Ovi <ovi.poddar2@gmail.com>
2021-02-18 12:22:23 +0600

    Vendor SigntUp html added

--- webapp/vendor/models.py
+++ webapp/vendor/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class TicketVendor(models.Model):
+
+    id = models.IntegerField(primary_key=True)
+    email = models.EmailField(null=False)
+    password = models.TextField(null=False)
+
+    name = models.TextField(null=False, max_length=100)
+    address   = models.CharField(null=False, max_length=100)
+    unique_hash = models.TextField()
+
+    def __str__(self):
+        return `Vendorid: ${id}, Email: ${email}`
+
